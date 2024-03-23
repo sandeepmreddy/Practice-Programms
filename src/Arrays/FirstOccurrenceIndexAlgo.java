ad5a0ac10660c0893f2ae88617ca0e927804c084
sandeep.mreddy <sandeep.mreddy@gmail.com>
2024-03-23 12:35:18 +0530

    programms

---
+++ src/Arrays/FirstOccurrenceIndexAlgo.java
@@ -0,0 +1,32 @@
+package Arrays;
+// Time complexity: O(nm)
+//Space complexity: O(1)
+public class FirstOccurrenceIndexAlgo {
+
+    public static void main(String[] args) {
+        String haystack = "Helloworldhiworld";
+        String needle = "world";
+
+
+        int index = strStr(haystack,needle);
+        System.out.println(index);
+    }
+
+    public static int strStr(String haystack, String needle) {
+        int m = needle.length();
+        int n = haystack.length();
+
+        for (int windowStart = 0; windowStart <= n - m; windowStart++) {
+            for (int i = 0; i < m; i++) {
+                if (needle.charAt(i) != haystack.charAt(windowStart + i)) {
+                    break;
+                }
+                if (i == m - 1) {
+                    return windowStart;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
